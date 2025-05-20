b343d83e73443510be9feeaf645a83c979a9015e
Staraguay <95930050+Staraguay@users.noreply.github.com>
2025-05-19 21:36:26 -0700

    final version

---
+++ setup_apache.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Definir la nueva carpeta del proyecto
+PROJECT_PATH="/var/www/assignment_2"
+
+# Cambiar permisos y propietario
+sudo chown -R apache:apache "$PROJECT_PATH"  # Para Amazon Linux / CentOS
+sudo chmod -R 755 "$PROJECT_PATH"
+
+# Modificar configuración de Apache
+APACHE_CONF="/etc/httpd/conf/httpd.conf"  # Para Amazon Linux / CentOS
+
+# Reemplazar la configuración de DocumentRoot
+sudo sed -i "s|DocumentRoot .*|DocumentRoot $PROJECT_PATH|g" "$APACHE_CONF"
+
+# Agregar directiva para permitir acceso
+sudo sed -i "/<Directory \\/var\\/www\\//,/<\\/Directory>/d" "$APACHE_CONF"
+echo -e "<Directory $PROJECT_PATH>\n    AllowOverride All\n    Require all granted\n</Directory>" | sudo tee -a "$APACHE_CONF"
+
+sudo systemctl restart httpd 2>/dev/null || sudo systemctl restart apache2
